80524498e8ada90f59d90da4741485a483a18e6b
EmersonPuebla <173473581+EmersonPuebla@users.noreply.github.com>
2026-05-07 00:55:31 -0400

    ✨add ProjectPage search and move mock data to const

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,28 +1,65 @@
-import { Container, Flex, Grid, TextField } from "@radix-ui/themes";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+import { Container, Flex, Grid, TextField, Text } from "@radix-ui/themes";
+import { InfoCircledIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { ProjectCard } from "@components/ProjectCard";
 import { Header } from "@components/Header";
 import { BlueprintBackground } from "@components/BlueprintBackground";
 
+// Mockup projects
+const PROJECTS = [
+  "Clinica Atlas",
+  "SOS Forestal",
+  "Agro Food Manager",
+  "Eduka",
+];
+
 export function ProjectsPage() {
+  const [search, setSearch] = useState("");
+
+  const filteredProjects = PROJECTS.filter((project) =>
+    project.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
     <>
       <BlueprintBackground>
         <Header></Header>
-        <Container size="3">
+        <Container
+          size="3"
+          style={{
+            marginLeft: "12px",
+            marginRight: "12px",
+          }}
+        >
           <Flex direction="column" gap="4">
-            <TextField.Root placeholder="Buscar proyecto…">
+            <TextField.Root
+              placeholder="Buscar proyecto…"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              size="3"
+            >
               <TextField.Slot>
                 <MagnifyingGlassIcon height="16" width="16" />
               </TextField.Slot>
             </TextField.Root>
 
-            <Grid columns={{ initial: "1", sm: "2", md: "3" }} gap="2">
-              <ProjectCard name="Clinica Atlas"></ProjectCard>
-              <ProjectCard name="SOS Forestal"></ProjectCard>
-              <ProjectCard name="Agro Food Manager"></ProjectCard>
-              <ProjectCard name="Eduka"></ProjectCard>
-            </Grid>
+            {filteredProjects.length > 0 ? (
+              <Grid columns={{ initial: "1", sm: "2", md: "3" }} gap="2">
+                {filteredProjects.map((project) => (
+                  <ProjectCard key={project} name={project}></ProjectCard>
+                ))}
+              </Grid>
+            ) : (
+              <Flex
+                justify="center"
+                align="center"
+                style={{ minHeight: "300px" }}
+              >
+                <Text size="3" color="gray" align="center">
+                  <InfoCircledIcon /> Sin coincidencias :(
+                </Text>
+              </Flex>
+            )}
           </Flex>
         </Container>
       </BlueprintBackground>
